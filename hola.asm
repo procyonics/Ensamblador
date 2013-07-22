aedbb9fcaa0686a070c68480ca407851217d1954
Roberto Cadena Vega <robblack00_7@hotmail.com>
2013-07-22 18:39:01 -0500

    Se termina de afinar los detalles de la plantilla

--- hola.asm
+++ hola.asm
@@ -2,14 +2,14 @@
 ;	Información del proyecto
 ;=============================================================================
 ;Nombre:		Hola Mundo
-;Descripción:	Lee el Dip switch conectado al PC y muestra el valor leído en
-;				el PB a través de 8 leds
-;Modelo:		PIC16F886 - Oscilador int - 4MHz
+;Descripción:	El microcontrolador dará como salida un 1 logico, cuando tenga
+;				como entrada 1 logico en dos de sus entradas.
+;Modelo:		PIC16F886 - Oscilador interno - 4MHz
 ;Autor:			robblack00_7
 ;Fecha:			22-Julio-2013
 ;Versión:		0.1
 ;=============================================================================
-;	Configuración
+;	Configuración para el compilador
 ;=============================================================================
 		LIST   	p=16F886			; Procesador utilizado
 		INCLUDE	"P16F886.INC" 		; Libreria de direcciones correspondientes
@@ -17,7 +17,6 @@
 ;=============================================================================
 ;	Área de equivalencias
 ;=============================================================================
-
 PORTA   	EQU     0X05    
 PORTB   	EQU     0X06    
 ESTADO  	EQU     0X03    
@@ -25,41 +24,43 @@ W       	EQU     0
 ;=============================================================================
 ;	Inicialización del microcontrolador
 ;=============================================================================
-			ORG		0x0000			; VECTOR RESET (DIR. DE LA MEMORIA FLASH)
+			ORG		0x0000			; Vector RESET, Dirección en la memoria
+									; FLASH
 INICIALIZA_MC
-			BANKSEL	TRISA			; CAMBIO AL BANCO DONDE SE ENCUENTRA TRSIA
-			MOVLW	B'11111111'		; ESCRIBE ESTE VALOR EN EL ACUMULADOR
-			MOVWF	PORTA			; MUEVE ESTE VALOR A PORTA (ENT 1 - SAL 0)
-			MOVLW	B'00000000'		; ESCRIBE ESTE VALOR EN EL ACUMULADOR
-			MOVWF	PORTB			; MUEVE ESTE VALOR A PORTB (ENT 1 - SAL 0)
+			BANKSEL	TRISA			; Cambio al banco del registro TRISA
+			MOVLW	B'11111111'		; Escribe este valor en el acumulador
+			MOVWF	TRISA			; Mueve este valor a TRISA (TRISB)
+			MOVLW	B'00000000'		; Entrada - 1
+			MOVWF	TRISB			; Salida - 0
 		
-			BANKSEL	OSCCON
-			MOVLW	B'1100000'		; ESCRIBE ESTE VALOR EN EL ACUMULADOR
-			MOVWF	OSCCON			; MUEVE ESTE VALOR AL REG. OSCCON (VEL=4MHz.)
+			BANKSEL	OSCCON			; Cambio al banco del registro OSCCON
+			MOVLW	B'1100000'		; Escribe este valor en el acumulador
+			MOVWF	OSCCON			; Mueve este valor al registro OSCCON
+									; '11000000' = 4 MHz
 
-			BANKSEL	ANSEL			; CAMBIO AL BANCO DONDE SE ENCUENTRA EL SFR ANSEL
-			CLRF	ANSELH			; CARGA ANSEL CON CEROS PARA CONFIGURAR LAS
-			MOVLW	B'0000000'		; ENTRADAS YA SEA COMO DIGITALES
-			MOVWF	ANSEL			; MUEVE ESTE VALOR AL REG. ANSEL (1 ANA., O DIG.)
+			BANKSEL	ANSELH			; Cambio al banco del registro ANSELH,
+			CLRF	ANSELH			; se borra el registro ANSELH y ANSEL
+			MOVLW	B'0000000'		; lo que configura las entradas como
+			MOVWF	ANSEL			; digitales
 	
-			BANKSEL	PORTA			; PASAMOS AL BANCO PARA TRABAJAR CON EL PORTA Y PORTB
-			CLRF 	PORTB			; LIMPIA EL REGITRO PORTB
+			BANKSEL	PORTB			; Cambio al banco del registro PORTB
+			CLRF 	PORTB			; Se borra el puerto de salida
 ;=============================================================================
 ;  	Programa principal
 ;=============================================================================
-INICIO	
-			MOVF	PORTA,W			; LEE ESTADO DEL PUERTO A Y COPIA EL VALOR EN W
-			MOVWF	PORTB			; MUEVE EL VALOR DE W AL PUERTO B
-			GOTO	INICIO			; SALTA A LA ETIQUETA "INICIO"
+INICIO
+			MOVF	PORTA,W			; Lee el estado de PORTA y lo copia en W
+			MOVWF	PORTB			; Mueve el valor de W a PORTB
+			GOTO	INICIO			; Salta a la etiqueta INICIO
 ;=============================================================================
 ;	Palabras de configuración del microcontrolador
 ;=============================================================================
-			ORG    	0x2007			; DIR. DE MEM. FLASH DE LA CONFIGURACION
-			DATA	H'20C4'			; VALOR PARA LA PALABRA DE CONFIGURACION
-			ORG    	0x2008			; WATCH DOG OFF; OSCILADOR INTERNO RA6 Y RA7 I/O;  
-			DATA	H'0600'			; PROTECCIÓN DE CÓDIGO OFF; ETC.
+			ORG    	0x2007			; Dirección en la memoria FLASH de la
+									; configuración
+			DATA	H'20C4'			; Valor de la palabra de configuración
+			ORG    	0x2008			; Watchdog OFF, Oscilador Interno, RA6 y
+			DATA	H'0600'			; RA7 I/O, protección de código OFF, etc.
 ;=============================================================================
 ;	Fin del programa
 ;=============================================================================
-		END
-;=============================================================================
+			END
